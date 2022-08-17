987ba8c9553263a183ceb5a56879d8c75f1778cf
rickyli888 <rickyli860@gmail.com>
2022-08-17 14:58:28 -0400

    Created Banner and Container

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className={"Banner"}>
+            <h1>Cisco Sextant</h1>
+        </div>
+        <div className={"container"}>
+            <h2>Container</h2>
+        </div>
     </div>
+
   );
 }
 
